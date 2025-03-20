057a80d2bd68685ec9a76a9874cb272e56da1858
abookvv <97209148+abookvv@users.noreply.github.com>
2025-03-20 11:40:43 +0300

    Add files via upload

---
+++ homework2.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class homework2 {
+
+    @org.junit.Test
+    @Test
+    public void testTrueCondition() {
+        // Проверяем, что true равно true
+        assertTrue(true, "Ожидается, что true будет истинным");
+    }
+}
