04cb1b1b7cba752c39398512a81299f6573ba04b
Anne <anna301488@gmail.com>
2025-05-26 11:03:59 +0300

    3 tests homework 2

--- src/test/java/org/example/tests/ExamplePageTest.java
+++ src/test/java/org/example/tests/ExamplePageTest.java
@@ -1,12 +1,18 @@
 package org.example.tests;
 
+import java.time.Duration;
 import com.codeborne.selenide.Configuration;
 import org.example.pages.ExamplePage;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.TimeoutException;
 
 import static com.codeborne.selenide.Selenide.title;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,9 +22,9 @@ public class ExamplePageTest {
     private ExamplePage page;
     @BeforeEach
     void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anne\\chromedriver-win64\\chromedriver.exe"); // Замените на фактический путь
-        Configuration.browser = "chrome"; // Убедитесь, что браузер установлен на Chrome
-//        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anne\\chromedriver-win64\\chromedriver.exe");
+        Configuration.browser = "chrome";
+        Configuration.timeout = 120000;
         page = new ExamplePage();
         page.openPage();
     }
@@ -31,10 +37,25 @@ public class ExamplePageTest {
         String searchQuery = "Java";
         page.searchFor(searchQuery);
         page.openFirstArticle();
-        assertTrue(page.getArticleTitle().toLowerCase().contains(searchQuery.toLowerCase()));
+
+        String articleTitle = page.getArticleTitle();
+        assertTrue(articleTitle.toLowerCase().contains(searchQuery.toLowerCase()),
+                "Заголовок статьи не содержит искомый текст: " + searchQuery);
     }
     @Test
     public void testLoginButtonVisible() {
         assertTrue(page.isLoginButtonVisible());
     }
+
+    @Test
+    public void testNavigationToNewsSection() {
+        page.clickNewsSection();
+        WebDriverWait wait = new WebDriverWait(WebDriverRunner.getWebDriver(), Duration.ofSeconds(30));
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Новости']")));
+        } catch (TimeoutException e) {
+            System.out.println("Элемент не появился на странице в течение 30 секунд");
+        }
+        assertTrue(WebDriverRunner.url().contains("/news") || WebDriverRunner.url().contains("/ru/news"),
+                "URL не содержит ожидаемый путь /news, текущий URL: " + WebDriverRunner.url());}
 }
